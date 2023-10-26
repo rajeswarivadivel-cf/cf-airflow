d00c05c1b8d212055e8ed2a6059db37c949ad591
Billy Chau <billy.chau@cashflows.com>
2023-10-26 23:34:31 +0100

    refactor(airflow): refactor s3_to_redshift.py

--- dags/common/s3_to_redshift.py
+++ dags/common/s3_to_redshift.py
@@ -8,6 +8,7 @@ from airflow import DAG
 from airflow.models import BaseOperator
 from airflow.models.baseoperator import chain
 from airflow.providers.amazon.aws.hooks.s3 import S3Hook
+from airflow.providers.amazon.aws.operators.s3 import S3DeleteObjectsOperator
 from airflow.providers.amazon.aws.transfers.s3_to_redshift import S3ToRedshiftOperator
 
 logger = logging.getLogger(__name__)
@@ -35,66 +36,48 @@ class S3FilesTransformOperator(BaseOperator):
 
     def execute(self, context):
         s3_hook = S3Hook()
-        keys = [key for key in s3_hook.list_keys(bucket_name=self.source_bucket, prefix=self.source_prefix) if
-                not key.endswith('/')]
+        s3_keys = s3_hook.list_keys(bucket_name=self.source_bucket, prefix=self.source_prefix)
+        s3_keys = [key for key in s3_keys if not key.endswith('/')]  # Skip folders.
+        logger.info(f'Found {len(s3_keys)} keys.')
         if self.wildcard_key:
-            keys = [key for key in keys if re.match(self.wildcard_key, key)]
-        context['task_instance'].xcom_push(key='source_bucket', value=self.source_bucket)
-        context['task_instance'].xcom_push(key='source_keys', value=keys)
-        for key in keys:
-            logger.info(f'Found {key}.')
+            s3_keys = [key for key in s3_keys if re.match(self.wildcard_key, key)]
+        context['ti'].xcom_push(key='source_bucket', value=self.source_bucket)
+        context['ti'].xcom_push(key='source_keys', value=s3_keys)
+        for s3_key in s3_keys:
+            logger.info(f'Processing \'{s3_key}\'.')
             with NamedTemporaryFile(mode='wb') as f_source, NamedTemporaryFile(mode='wb') as f_dest:
-                source_s3_key_object = s3_hook.get_key(bucket_name=self.source_bucket, key=key)
-                source_s3_key_object.download_fileobj(Fileobj=f_source)
+                s3_obj = s3_hook.get_key(bucket_name=self.source_bucket, key=s3_key)
+                s3_obj.download_fileobj(Fileobj=f_source)
                 f_source.flush()
-                self.transform_func(f_source_name=f_source.name, f_dest_name=f_dest.name, source_key=key)
+                self.transform_func(f_source_name=f_source.name, f_dest_name=f_dest.name, source_key=s3_key)
                 f_dest.flush()
-                path = Path(key)
+                path = Path(s3_key)
                 dest_fn = path.name.replace(path.suffix, '.csv')
                 s3_hook.load_file(filename=f_dest.name, key=f's3://{self.dest_bucket}/{self.dest_prefix}/{dest_fn}')
-
-
-class RemoveTempFilesOperator(BaseOperator):
-    def __init__(
-            self,
-            bucket_name: str,
-            prefix: str,
-            **kwargs):
-        super().__init__(**kwargs)
-        self.bucket_name = bucket_name
-        self.prefix = prefix
-
-    def execute(self, context):
-        s3_hook = S3Hook()
-        keys = s3_hook.list_keys(bucket_name=self.bucket_name, prefix=self.prefix)
-        s3_hook.delete_objects(bucket=self.bucket_name, keys=keys)
 
 
 class ArchiveOperator(BaseOperator):
     def __init__(
             self,
-            archive_bucket_name: str,
+            archive_bucket: str,
             archive_prefix: str,
-            transform_task_id: str = None,
             *args, **kwargs):
         super().__init__(*args, **kwargs)
-        self.archive_bucket_name = archive_bucket_name
+        self.archive_bucket = archive_bucket
         self.archive_prefix = archive_prefix
-        self.transform_task_id = transform_task_id
 
     def execute(self, context):
-        source_bucket = context['ti'].xcom_pull(task_ids=self.transform_task_id or TRANSFORM_TASK_ID,
-                                                key='source_bucket')
-        source_keys = context['ti'].xcom_pull(task_ids=self.transform_task_id or TRANSFORM_TASK_ID, key='source_keys')
+        source_bucket = context['ti'].xcom_pull(task_ids=TRANSFORM_TASK_ID, key='source_bucket')
+        source_keys = context['ti'].xcom_pull(task_ids=TRANSFORM_TASK_ID, key='source_keys')
         s3_hook = S3Hook()
-        for key in source_keys:
+        for source_key in source_keys:
             s3_hook.copy_object(
                 source_bucket_name=source_bucket,
-                source_bucket_key=key,
-                dest_bucket_name=self.archive_bucket_name,
-                dest_bucket_key=f'{self.archive_prefix}/{Path(key).name}'
+                source_bucket_key=source_key,
+                dest_bucket_name=self.archive_bucket,
+                dest_bucket_key=f'{self.archive_prefix}/{Path(source_key).name}'
             )
-        s3_hook.delete_objects(bucket=source_bucket, keys=source_keys)
+            s3_hook.delete_objects(bucket=source_bucket, keys=source_key)
 
 
 def s3_to_redshift_pipeline(
@@ -128,21 +111,21 @@ def s3_to_redshift_pipeline(
         copy_options=['REMOVEQUOTES']
     )
 
-    remove_temp_files = RemoveTempFilesOperator(
+    remove_tmp = S3DeleteObjectsOperator(
         dag=dag,
-        task_id='remove_temp_files',
-        bucket_name=s3_bucket,
+        task_id='remove_tmp',
+        bucket=s3_bucket,
         prefix=f'{s3_prefix}/tmp'
     )
 
     archive = ArchiveOperator(
         dag=dag,
         task_id='archive',
-        archive_bucket_name=s3_bucket,
+        archive_bucket=s3_bucket,
         archive_prefix=f'{s3_prefix}/archive'
     )
 
-    tasks = [transform, s3_to_redshift, remove_temp_files, archive]
+    tasks = [transform, s3_to_redshift, remove_tmp, archive]
 
     if pre_operator:
         tasks = [pre_operator()] + tasks
