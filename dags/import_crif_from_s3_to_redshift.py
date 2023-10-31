d699603f9d1028c09fc8f93c3d51e8fc646ef23b
rajeswarivadivel-cf <rajeswari.vadivel@cashflows.com>
2023-10-31 13:22:37 +0000

    crif report daily processing

--- dags/import_crif_from_s3_to_redshift.py
+++ dags/import_crif_from_s3_to_redshift.py
@@ -3,9 +3,13 @@ from airflow import DAG
 from airflow.decorators import dag,task
 from airflow.models import Variable
 <<<<<<< HEAD
+<<<<<<< HEAD
 from airflow.exceptions import AirflowFailException
 =======
 >>>>>>> 3be7c3f (add crif reports)
+=======
+from airflow.exceptions import AirflowFailException
+>>>>>>> f235b6c (crif report daily processing)
 from airflow.models.taskinstance import TaskInstance
 from airflow.providers.amazon.aws.hooks.s3 import S3Hook
 from airflow.providers.amazon.aws.hooks.redshift_sql import RedshiftSQLHook 
@@ -13,9 +17,13 @@ from airflow.providers.amazon.aws.transfers.s3_to_redshift import S3ToRedshiftOp
 from airflow.operators.empty import EmptyOperator
 from airflow.utils.email import send_email
 <<<<<<< HEAD
+<<<<<<< HEAD
 from airflow.macros import ds_format
 =======
 >>>>>>> 3be7c3f (add crif reports)
+=======
+from airflow.macros import ds_format
+>>>>>>> f235b6c (crif report daily processing)
 
 import logging
 import sys
@@ -39,16 +47,21 @@ default_args = {
 	'depends_on_past': False,
 	'email': ['data.analytics@cashflows.com'],
 <<<<<<< HEAD
+<<<<<<< HEAD
 	'email_on_failure': True,
 =======
 	'email_on_failure': False,
 >>>>>>> 3be7c3f (add crif reports)
+=======
+	'email_on_failure': True,
+>>>>>>> f235b6c (crif report daily processing)
 	'email_on_retry': False,
 	'retries': 0,
 	'retry_delay': timedelta(minutes=5)
 }
 
 source_bucket = Variable.get('s3_sftp_mirror')
+<<<<<<< HEAD
 <<<<<<< HEAD
 source_prefix = Variable.get('crif_source_prefix')
 dest_bucket =Variable.get('s3_bucket_name')
@@ -79,30 +92,42 @@ def task_failure_alert(context):
 	task_key=task_key
 =======
 destination_prefix = Variable.get('crif_dest_prefix')
+=======
+>>>>>>> f235b6c (crif report daily processing)
 source_prefix = Variable.get('crif_source_prefix')
-destination_bucket =Variable.get('s3_bucket_name')
+dest_bucket =Variable.get('s3_bucket_name')
+dest_prefix = Variable.get('crif_dest_prefix')
+transformed_prefix = f'{dest_prefix}/transformed'
+archive_prefix = f'{dest_prefix}/archive'
 s3_hook = S3Hook()
 s3 = boto3.client("s3")
 
+
+
 def task_failure_alert(context):
-	exception_type = context.get('exception', {}).get('type', 'UnknownError')
-	exception_message = context.get('exception', {}).get('exc_message', 'Unknown error occurred')
+	exception_type = context['exception']
+	#exception_type = context.get('exception', {}).get('type', 'UnknownError')
+	#exception_message = context.get('exception', {}).get('exc_message', 'Unknown error occurred')
 	task_key =  {context['task_instance_key_str']}
-	logger.info(f"Task has failed, task_instance_key_str: {context['task_instance_key_str']}")
+	logger.info(f"Task failed, {context['task_instance_key_str']}")
 	subject = 'Alert : Error in CRIF daily data load job'
 	html_content = """<html> <head></head> <body>
-						    <p style="color: red;">An error occurred in daily CRIF data load.</p>
-						    <p>Error type: {exception_type}</p>
-						    <p>Error message: {exception_message}</p>
-						    <p>Task has failed, task_instance_key_str: {task_key}</p>
-						    <p>Please correct it and re-run the job.</p>
+							<p style="color: red;">An error occurred in daily CRIF data load.</p>
+							<p>Error : {exception_type}</p>
+							<p>Task has failed, task_instance_key_str: {task_key}</p>
+							<p>Please correct it and re-run the job.</p>
 						</body>
 						</html>"""
 	formatted_html = html_content.format(
+<<<<<<< HEAD
     exception_type=exception_type,
     exception_message=exception_message,
     task_key=task_key
 >>>>>>> 3be7c3f (add crif reports)
+=======
+	exception_type=exception_type,
+	task_key=task_key
+>>>>>>> f235b6c (crif report daily processing)
 	)
 	send_email(to=['data.analytics@cashflows.com'],
 			subject=subject,
@@ -114,10 +139,14 @@ def task_failure_alert(context):
 	default_args=default_args,
 	description='It copies daily CRIF fast onboaeding data from  SFTP (S3 bucket) to redshift',
 <<<<<<< HEAD
+<<<<<<< HEAD
 	start_date= datetime(2023, 10, 31, 15, 00),
 =======
 	start_date= datetime(2023, 10, 20, 13, 30),
 >>>>>>> 3be7c3f (add crif reports)
+=======
+	start_date= datetime(2023, 10, 31, 15, 00),
+>>>>>>> f235b6c (crif report daily processing)
 	schedule=None,
 	tags=['s3', 'CRIF'],
 	catchup=False ,
@@ -128,23 +157,33 @@ def taskflow():
 
 	@task(task_id="read_transform_crif_source_file")
 <<<<<<< HEAD
+<<<<<<< HEAD
+=======
+>>>>>>> f235b6c (crif report daily processing)
 	def read_transform_crif_source_file(ds='{{ ds_nodash }}',ti: TaskInstance = None,**kwargs):
 		print(ds)
 		file_date = ds_format( ds, "%Y-%m-%d", "%d%m%Y")
 		print(file_date)
 		source_keys = wr.s3.list_objects(f's3://{source_bucket}/{source_prefix}/*{file_date}*')
+<<<<<<< HEAD
 =======
 	def read_transform_crif_source_file():
 		source_keys = wr.s3.list_objects(f's3://{source_bucket}/{source_prefix}/', suffix = '.csv')
 >>>>>>> 3be7c3f (add crif reports)
+=======
+>>>>>>> f235b6c (crif report daily processing)
 		logger.info(source_keys)
 		for i  in range(0, len(source_keys)):
 			file = source_keys[i]
 			filename = file.split('/')[-1]
 <<<<<<< HEAD
+<<<<<<< HEAD
 			ti.xcom_push(key='file', value=filename)
 =======
 >>>>>>> 3be7c3f (add crif reports)
+=======
+			ti.xcom_push(key='file', value=filename)
+>>>>>>> f235b6c (crif report daily processing)
 			logger.info(f'Processing {file}.')
 			df_raw = wr.s3.read_csv(file,sep = ',',header=0,index_col=False,path_suffix='.csv')
 			logger.info(df_raw.head())            
@@ -163,6 +202,7 @@ def taskflow():
 			logger.info(df_final.info())
 			logger.info('Write the transformed file to transformed path')
 <<<<<<< HEAD
+<<<<<<< HEAD
 			dtypes = {'Score': 'int',  'Average Delivery Days' : 'int'}
 			wr.s3.to_csv(df = df_final, path = f's3://{dest_bucket}/{transformed_prefix}/transformed_{filename}',index=False,dtype=dtypes)
 
@@ -179,16 +219,26 @@ def taskflow():
 =======
 			dtypes = {'Score': 'int', 'Monthly Card Sales': 'int', 'Average Delivery Days' : 'int'}
 			wr.s3.to_csv(df = df_final, path = f's3://{destination_bucket}/{destination_prefix}/transformed_{filename}',index=False,dtype=dtypes)
+=======
+			dtypes = {'Score': 'int',  'Average Delivery Days' : 'int'}
+			wr.s3.to_csv(df = df_final, path = f's3://{dest_bucket}/{transformed_prefix}/transformed_{filename}',index=False,dtype=dtypes)
+
+>>>>>>> f235b6c (crif report daily processing)
 
 
 	@task(task_id="write_to_redshift")
-	def write_to_redshift(ti: TaskInstance = None):
+	def write_to_redshift(ti: TaskInstance = None,**kwargs):
 		s3_hook = S3Hook()
 		credentials = s3_hook.get_credentials()
+		file_t= ti.xcom_pull(task_ids='read_transform_crif_source_file',key='file')
 		sql_queries = [
 				"delete  from staging.crif_fast_onboarding_source_stg",
+<<<<<<< HEAD
 	   			 f"""copy staging.crif_fast_onboarding_source_stg  from  's3://{destination_bucket}/{destination_prefix}/transformed_' 
 >>>>>>> 3be7c3f (add crif reports)
+=======
+	   			 f"""copy staging.crif_fast_onboarding_source_stg  from  's3://{dest_bucket}/{transformed_prefix}/transformed_{file_t}' 
+>>>>>>> f235b6c (crif report daily processing)
 						CREDENTIALS  'aws_access_key_id={credentials.access_key};aws_secret_access_key={credentials.secret_key};token={credentials.token}' 
 						IGNOREHEADER 1
 						DATEFORMAT 'YYYY-MM-DD'
@@ -210,6 +260,7 @@ def taskflow():
 		except Exception as e:
 			print(f"An error occurred: {e}")
 <<<<<<< HEAD
+<<<<<<< HEAD
 			raise  AirflowFailException(f"Database error: {str(e)}")	
 		else:
 			print('Data load is sucessful')
@@ -229,14 +280,30 @@ def taskflow():
 	start >> read_transform_crif_source_file() >> write_to_redshift() >> archive_file() >> end
 =======
 			raise		
+=======
+			raise  AirflowFailException(f"Database error: {str(e)}")	
+>>>>>>> f235b6c (crif report daily processing)
 		else:
-			#wr.s3.delete_objects('s3://cf-analytics-team/cf-datalake/datalake_etl/datalake')
 			print('Data load is sucessful')
+
+	@task(task_id="archive_file")
+	def archive_file(ti: TaskInstance = None,**kwargs):
+		file_t= ti.xcom_pull(task_ids='read_transform_crif_source_file',key='file')
+		print(file_t)
+		wr.s3.copy_objects(paths=[f's3://{dest_bucket}/{transformed_prefix}/transformed_{file_t}'], source_path=f's3://{dest_bucket}/{transformed_prefix}/',target_path=f's3://{dest_bucket}/{archive_prefix}/')
+		logger.info('File archived after processing {file_t}')
+		wr.s3.delete_objects(f's3://{dest_bucket}/{transformed_prefix}/*{file_t}')
+		logger.info('Remove the file {file_t} from transformed directory')
+
 
 	end = EmptyOperator(task_id='end')
 
+<<<<<<< HEAD
 	start >> read_transform_crif_source_file() >> write_to_redshift() >> end
 >>>>>>> 3be7c3f (add crif reports)
+=======
+	start >> read_transform_crif_source_file() >> write_to_redshift() >> archive_file() >> end
+>>>>>>> f235b6c (crif report daily processing)
 
 taskflow()	
 
